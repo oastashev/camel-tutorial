8a8d76bec91df620d00c74085e814b5b111d4b35
Oleg Astashev <o.astashev@cardpay.ru>
2015-04-09 13:05:46 +1000

    Removed target folder

--- target/classes/db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE geoip (
-    ip varchar(15) NOT NULL,
-    added TIMESTAMP DEFAULT NOW(),
-    country varchar(255) NOT NULL,
-    city varchar(255),
-    service varchar(255),
-    constraint geoip_pk PRIMARY KEY (ip)
-);
